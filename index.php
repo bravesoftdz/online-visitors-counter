b66f519e1d3b26cb50a0e139e8ac3ed7c02c926e
vagrant <vagrant@homestead>
2017-10-01 10:14:26 +0000

    add redis driver

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 require 'vendor/autoload.php';
 
-use Dykyi\Driver\MySQLDB;
+use Dykyi\Driver\RedisDB;
 use Dykyi\DBFactory;
 use Dykyi\VisitorsCounter;
 
 $dbFactory = new DBFactory();
-$dbFactory->setDriver(\Dykyi\Driver\RedisDB::class);
+$dbFactory->setDriver(RedisDB::class);
 $dataBase   = $dbFactory->makeDB(['127.0.0.1','','','']);
 $repository = $dbFactory->getRepository($dataBase);
 echo VisitorsCounter::getCount($repository);
